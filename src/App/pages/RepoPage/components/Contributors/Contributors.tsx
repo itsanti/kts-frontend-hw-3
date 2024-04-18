c6c4cb04223fd43fa2964d3eff7acea8bfc3bb97
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2024-04-18 23:10:21 +0300

    Add Contributors

--- src/App/pages/RepoPage/components/Contributors/Contributors.tsx
+++ src/App/pages/RepoPage/components/Contributors/Contributors.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Text from "components/Text";
+import contributors from "config/mocks/contributors";
+import repoUser from "config/mocks/user";
+import styles from './Contributors.module.scss';
 
 const Contributors: React.FC = () => {
     return (
-        <ul>
-            <li>stars</li>
-            <li>watching</li>
-            <li>forks</li>
-        </ul>
+        <div className={styles.root}>
+            <Text view="p-18" className={styles.header}>Contributors</Text>
+            <ul className={styles.list}>
+                {contributors.map((user) => (
+                    <li key={user.id}>
+                        <img className={styles.avatar} src={user.avatar_url} alt={user.login} />
+                        <Text view="p-16" className={styles.user}>{user.login}</Text> <Text view="p-16" color="secondary">{repoUser.name}</Text>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
